a4205701a9402211c5d58ffffe8fea80f9120339
Pratap Singh <pratap.singh@pysolutions.in>
2016-06-03 14:34:14 +0530

    Removed commented out code and print statement

--- product.py
+++ product.py
@@ -8,31 +8,4 @@ class on_change_function(models.Model):
     
     @api.onchange('list_price')
     def _cal_sale_price(self):
-       # res = {}
-        print "----------------------------------"
         self.standard_price = self.list_price + 10
-        #res = {
-                #'standard_price' : total
-        #}
-        #res['standard_price'] = total
-        #print "------------------------------------"
-        #print res
-        #return res
-        #setattr(self, 'standard_price', total)
-        #setattr(self, 'weight', total)
-        
-    #def _on_change_price(self, cr, user, ids, list_price, context = None):
-        
-     #   res = {}
-        
-      #  total = list_price + 10
-       # res = {
-        #    'value':{
-        #         'standard_price' : total  
-         #   }
-        #}
-        #return res    
-    
-    
-    
-    
--- sequence_generate_old_api_code.py
+++ sequence_generate_old_api_code.py
@@ -1,6 +1,10 @@
+# this py file is not related to this module.
+# Only for learning purpose
+
+# for generating sequence no in old api when click on save button
 @api.onchange('name_')
 @api.depends('name_')    
-def _create(self, cr, uid, vals, context=None):
+def create(self, cr, uid, vals, context=None):
     if context is None:
         context = {}
     
@@ -9,3 +13,14 @@ def _create(self, cr, uid, vals, context=None):
     self._name = super(CallForBids, self).create(cr, uid, vals, context=ctx)
     return new_id
 
+# structure of onchange method that return a value in old api      
+def _on_change_price(self, cr, user, ids, list_price, context = None):
+    res = {}
+
+    total = list_price + 10
+    res = {
+        'value':{
+            'standard_price' : total  
+            }
+        }
+    return res
